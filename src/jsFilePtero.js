d87c671307fa76c0f3821aaf944b89dc74a89382
Juni <juniortienoue01@gmail.com>
2024-04-28 18:06:56 +0100

    28/04/2024 jour-nuit avec interfaces

---
+++ src/jsFilePtero.js
@@ -0,0 +1,12 @@
+var heureActuelle=new Date();
+var debutJour=new Date();
+var debutNuit=new Date();
+
+debutJour.setHours(6,0,0);
+debutNuit.setHours(19,0,0);
+
+if(heureActuelle>=debutJour && heureActuelle<debutNuit){ document.body.style.backgroundImage = "url('img/forest.png')";}
+else {document.body.style.backgroundImage = "url('img/forest_night.png')";}
+
+
+
