4a219ac934b52a6da4fa04252da4aac294edcf90
Chrimika <108604904+Chrimika@users.noreply.github.com>
2024-04-29 02:08:52 +0100

    Integration Nuit_Jour

--- src/App.js
+++ src/App.js
@@ -1,88 +1,78 @@
 function resoudreEquation() {
-    const a = parseFloat(document.getElementById('a').value);
-    const b = parseFloat(document.getElementById('b').value);
-    const c = parseFloat(document.getElementById('c').value);
+  const a = parseFloat(document.getElementById("a").value);
+  const b = parseFloat(document.getElementById("b").value);
+  const c = parseFloat(document.getElementById("c").value);
 
-    if (isNaN(a) || isNaN(b) || isNaN(c)) {
-        document.getElementById('resultat').textContent = 'Les coefficients doivent être des nombres réels.';
+  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    document.getElementById("resultat").textContent =
+      "Les coefficients doivent être des nombres réels.";
+  } else {
+    const resultat = resoudreEquationQuadratique(a, b, c);
+    let message = "";
+    const vitesse = resultat.vitesse;
+    console.log(vitesse);
+    localStorage.setItem("vitesse", vitesse);
+    if (resultat.discriminantPositif) {
+      message =
+        "Solutions réelles : " +
+        resultat.solutions.join(" ; ") +
+        "   Viteese = " +
+        resultat.vitesse;
+      window.location.href = "dino.html";
     } else {
-        const resultat = resoudreEquationQuadratique(a, b, c);
-        let message = '';
-        const vitesse = resultat.vitesse;
-        console.log(vitesse);
-        localStorage.setItem('vitesse', vitesse);
-        if (resultat.discriminantPositif) {
-            message = 'Solutions réelles : ' + resultat.solutions.join(' ; ')+'   Viteese = '+resultat.vitesse;
-            window.location.href = "dino.html";
-        } else {
-            message = 'Solutions complexes : '+resultat.solutions.join(' ; ')+'   Viteese = '+resultat.vitesse;
-            window.location.href = "ptero.html";
-        }
-        document.getElementById('resultat').textContent = message;
-        
+      message =
+        "Solutions complexes : " +
+        resultat.solutions.join(" ; ") +
+        "   Viteese = " +
+        resultat.vitesse;
+      window.location.href = "ptero.html";
     }
+    document.getElementById("resultat").textContent = message;
+  }
 }
 
-
 function resoudreEquationQuadratique(a, b, c) {
-    const discriminant = b * b - 4 * a * c;
-    let solutions = [];
-    let discriminantPositif = true;
-    let vitesse=0;
+  const discriminant = b * b - 4 * a * c;
+  let solutions = [];
+  let discriminantPositif = true;
+  let vitesse = 0;
 
-    if (discriminant >= 0) {
-        let x1=(-b + Math.sqrt(discriminant)) / (2 * a);
-        let x2=(-b - Math.sqrt(discriminant)) / (2 * a);
-        solutions.push(x1);
-        solutions.push(x2);
-        vitesse=Math.sqrt(x1*x1+x2*x2);
-    } else {
-        discriminantPositif = false;
-        let x=-b/(2*a);
-        let y=Math.sqrt(-discriminant) / (2 * a);
-        let z1=x+' + i'+y;
-        let z2=x+' - i'+y
-        solutions.push(z1);
-        solutions.push(z2);
-        vitesse=Math.sqrt(x*x+y*y);
-    }
+  if (discriminant >= 0) {
+    let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    solutions.push(x1);
+    solutions.push(x2);
+    vitesse = Math.sqrt(x1 * x1 + x2 * x2);
+  } else {
+    discriminantPositif = false;
+    let x = -b / (2 * a);
+    let y = Math.sqrt(-discriminant) / (2 * a);
+    let z1 = x + " + i" + y;
+    let z2 = x + " - i" + y;
+    solutions.push(z1);
+    solutions.push(z2);
+    vitesse = Math.sqrt(x * x + y * y);
+  }
 
-    const resultat = {
-        solutions: solutions,
-        discriminantPositif: discriminantPositif,
-        vitesse: vitesse
-    };
+  const resultat = {
+    solutions: solutions,
+    discriminantPositif: discriminantPositif,
+    vitesse: vitesse,
+  };
 
-    return resultat;
+  return resultat;
 }
 
 /*parcours du dino*/
 var elem = document.getElementById("telem");
 var position = 0;
-var vitesse = localStorage.getItem('vitesse');
-alert(localStorage.getItem('vitesse'));
-moveElement();
+var vitesse = localStorage.getItem("vitesse");
+
 
 function moveElement() {
-    
-    position += vitesse;
-    elem.style.left = position + "px";
-    requestAnimationFrame(moveElement);
+  position += parseInt(vitesse)*0.5;
+  elem.style.left = position + "px";
+  requestAnimationFrame(moveElement);
 }
 
-
-
-
-// window.addEventListener('load', function() {
-//     var parent = document.getElementById('parent');
-//     var elem = document.getElementById('elem');
-    
-//     // Obtenez les dimensions du parent
-//     // var parentWidth = parent.offsetWidth;
-//     // var parentHeight = parent.offsetHeight;
-    
-//     // Ajustez la taille de l'élément pour correspondre à celle du parent
-//     elem.style.width = parentWidth + 'px';
-//     elem.style.height = parentHeight + 'px';
-// });
-
+moveElement();
--- src/jsFileDino.js
+++ src/jsFileDino.js
@@ -1,11 +1,12 @@
-var heureActuelle=new Date();
-var debutJour=new Date();
-var debutNuit=new Date();
+var heureActuelle = new Date();
+var debutJour = new Date();
+var debutNuit = new Date();
 
-debutJour.setHours(6,0,0);
-debutNuit.setHours(19,0,0);
+debutJour.setHours(6, 0, 0);
+debutNuit.setHours(19, 0, 0);
 
-if(heureActuelle>=debutJour && heureActuelle<debutNuit){document.body.style.backgroundImage = "url('img/savana.png')";}
-else {document.body.style.backgroundImage = "url('img/savana_night.png')";}
-
-
+if (heureActuelle >= debutJour && heureActuelle < debutNuit) {
+  document.body.style.backgroundImage = "url('img/savana.png')";
+} else {
+  document.body.style.backgroundImage = "url('img/savana_night.png')";
+}
