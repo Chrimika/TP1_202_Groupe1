a6a31e2033853668744dd8440756405565622c15
Chrimika <108604904+Chrimika@users.noreply.github.com>
2024-04-30 03:42:29 +0100

    Mini Corrections urls images

--- jsFilePtero.js
+++ jsFilePtero.js
@@ -6,7 +6,7 @@ debutJour.setHours(6, 0, 0);
 debutNuit.setHours(19, 0, 0);
 
 if (heureActuelle >= debutJour && heureActuelle < debutNuit) {
-  document.body.style.backgroundImage = "url('img/forest.png')";
+  document.body.style.backgroundImage = "url('../img/forest.png')";
 } else {
-  document.body.style.backgroundImage = "url('img/forest_night.png')";
+  document.body.style.backgroundImage = "url('../img/forest_night.png')";
 }
